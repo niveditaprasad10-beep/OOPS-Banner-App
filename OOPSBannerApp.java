4cbd1795c5db81fe42a10584e7f4ad8eae86660b
Nivedita Prasad <niveditaprasad10@gmail.com>
2026-02-24 14:38:23 +0530

    UC2-Banner print

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,6 +2,15 @@ public class OOPSBannerApp
 {
     public static void main(String[] args) 
     {
-        System.out.println("OOPS");
+
+        System.out.println("  ***      ***    ******    ******  ");
+        System.out.println(" ** **    ** **   **    **  **      ");
+        System.out.println("**   **  **   **  **    **  **      ");
+        System.out.println("**   **  **   **  ******     ****** ");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println(" ** **    ** **   **        **    **");
+        System.out.println("  ***      ***    **         ****** ");
+
     }
 }
